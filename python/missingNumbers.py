5ad999522753f726da8442eab3d1306302f1fc1a
Aloysius Riki <68075271+aloysius-riki@users.noreply.github.com>
2022-06-05 20:01:18 +0300

    first problem

---
+++ python/missingNumbers.py
@@ -0,0 +1,6 @@
+"""
+Given a list of numbers ordered in ascending order, write a function called missingNumbers
+in Python that finds the missing
+numbers in the list. For instance, if the first list is [1,2,6,7,10], the function
+will return [3,4,5,8,9].
+"""
