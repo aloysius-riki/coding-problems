595a5dccbc8c070c572bb71941aa95cfcdb6f7b5
Aloysius Riki <68075271+aloysius-riki@users.noreply.github.com>
2022-06-05 20:11:54 +0300

    add solution for missingNumbers problem

--- python/missingNumbers.py
+++ python/missingNumbers.py
@@ -4,3 +4,21 @@ in Python that finds the missing
 numbers in the list. For instance, if the first list is [1,2,6,7,10], the function
 will return [3,4,5,8,9].
 """
+
+import sys
+
+def missingNumbers(numbers):
+    missingNumbersList = []
+    numbers = [int(x) for x in numbers]
+    numbers.sort()
+    for i in range(1, numbers[-1] + 1):
+        if i not in numbers:
+            missingNumbersList.append(i)
+    return missingNumbersList
+
+def main():
+    numbers = sys.argv[1:]
+    print(missingNumbers(numbers))
+
+if __name__ == '__main__':
+    main()
